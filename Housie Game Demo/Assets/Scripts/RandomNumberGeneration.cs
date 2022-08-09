e33daf160244815da46640f082a9a3c19f2274b0
deepbajaj0211998 <47936906+deepbajaj0211998@users.noreply.github.com>
2022-08-09 15:31:59 -0700

    Updated Version of the Game.

--- Housie Game Demo/Assets/Scripts/RandomNumberGeneration.cs
+++ Housie Game Demo/Assets/Scripts/RandomNumberGeneration.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomNumberGeneration : MonoBehaviour
 {
 
-
+    public int num;
+    public GameObject numberText;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,13 @@ public class RandomNumberGeneration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    public void OnClick_NumberGenerator()
+    {
+        num = Random.Range(0, 99);
+        numberText.GetComponent<Text>().text = "Number is: " + num;
+    }
+
 }
